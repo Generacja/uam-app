d62d1c571b2f88118914ab6836dafde23180bf6c
mprendki <mateusz.prendki@vizlib.com>
2022-11-17 18:22:08 +0100

    change detection

--- src/app/core/api/facts.service.ts
+++ src/app/core/api/facts.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FactsService {
-  // https://catfact.ninja/facts?limit=10
   constructor(private httpClient: HttpClient) { }
 
-  getFacts() {
+  getFacts(): Observable<any> {
     return this.httpClient.get('https://catfact.ninja/facts?limit=10');
   }
 
--- src/app/features/main-page/_features/article-list/article-list.component.ts
+++ src/app/features/main-page/_features/article-list/article-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FactsService} from "../../../../core/api/facts.service";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss']
 })
-export class ArticleListComponent implements OnInit {
+export class ArticleListComponent {
+  public test = null;
+  constructor(private factsService: FactsService, private changeDetectorRef: ChangeDetectorRef) {
+    this.fetchCatFacts();
+  }
 
-  constructor() { }
 
-  ngOnInit(): void {
+  async fetchCatFacts() {
+    this.test = await firstValueFrom(this.factsService.getFacts());
+    this.changeDetectorRef.detectChanges();
+    console.log(this.test);
   }
 
 }
